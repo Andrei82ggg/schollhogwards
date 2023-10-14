6159d768a19a259490a95fd96c52bfce2eb7ce6a
Андрей <ustu1982@mail.ru>
2023-10-14 16:11:42 +0500

    end

---
+++ src/test/java/ru/hogwarts/skypro/school/skypro/service/FacultyServiceTest.java
@@ -0,0 +1,58 @@
+package ru.hogwarts.skypro.school.skypro.service;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import ru.hogwarts.skypro.school.skypro.model.Faculty;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class FacultyServiceTest {
+    FacultyService service = new FacultyService();
+
+    @Test
+    void testAdd() {
+        var result = service.add(new Faculty(null, "test", "test_color"));
+        assertEquals(result.getId(), 0);
+        assertEquals(result.getName(), "test");
+        assertEquals(result.getColor(), "test_color");
+    }
+
+    @Test
+    void testGet() {
+        var faculty = service.add(new Faculty(null, "test", "test_color"));
+        var result = service.get(faculty.getId());
+        assertEquals(result.getName(), "test");
+        assertEquals(result.getColor(), "test_color");
+    }
+
+    @Test
+    void testRemove() {
+        var faculty = service.add(new Faculty(null, "test1", "white"));
+        assertNotNull(service.get(faculty.getId()));
+        assertTrue(service.remove(faculty.getId()));
+        service.remove(faculty.getId());
+        assertNull(service.get(faculty.getId()));
+    }
+
+    @Test
+    void testUpdate() {
+        var result1 = service.update(new Faculty(9999L, "updated", "updated_color"));
+        assertNull(result1);
+        var addedFaculty = service.add(new Faculty(null, "test", "test_color"));
+        addedFaculty.setName("updated_name");
+        addedFaculty.setColor("updated_color");
+        var result2 = service.update(addedFaculty);
+        assertEquals(result2.getName(), "updated_name");
+        assertEquals(result2.getColor(), "updated_color");
+
+    }
+
+    @Test
+    void testFilterByAge() {
+        var added1 = service.add(new Faculty(null, "test1", "white"));
+        var added2 = service.add(new Faculty(null, "test2", "green"));
+        var added3 = service.add(new Faculty(null, "test3", "Green"));
+        var result = service.filterByColor("Green");
+        Assertions.assertThat(result).containsExactly(added2, added3);
+    }
+}
